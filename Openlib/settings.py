ae5db80079414e743a47a0b267fc7614e857e0be
Faiz Shaikh <94229675+Faiz-Shaikh-001@users.noreply.github.com>
2026-09-09 21:06:32 +0530

    Update settings.py

--- Openlib/settings.py
+++ Openlib/settings.py
@@ -27,10 +27,13 @@ EMAIL_PORT = EMAIL_PORT
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-0johf9i^pwqt!n*8g)(yqc+7200u!!or^$g-c--jtf4@r&07h4')
+SECRET_KEY = os.environ.get('SECRET_KEY')
+
+if not SECRET_KEY:
+    raise RuntimeError("SECRET_KEY environment variable is required")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = []
 
